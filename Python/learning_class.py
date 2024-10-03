848cbbc657ddc14239baa2a6a737ffdeff101dd6
BeneX Drake <benex@hotmail.de>
2024-10-03 10:46:16 +0200

    create for teaching python

---
+++ Python/learning_class.py
@@ -0,0 +1,10 @@
+class Calculator():
+    
+    def __init__(self):
+        pass
+
+    def add(self, a:int|float, b:int|float):
+        return a + b
+
+    def multi(self, a:int|float, b:int|float):
+        return a * b
