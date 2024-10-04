de460a3157e59ba7621b6c0f0a39b76f06a2e4a2
BeneX Drake <benex@hotmail.de>
2024-10-04 14:32:40 +0200

    adding create function and testing in main

---
+++ Python/To-Do/office_tasklist.py
@@ -0,0 +1,43 @@
+import datetime
+
+class OfficeTasklist:
+    
+    def __init__(self):
+        self.__todoList = []
+        
+    # Feature 1 add task
+    
+    ## 1. Nutze die `input`-Funktion, um den User nach einer Büroaufgabe zu fragen, z.B.: „Bitte gib eine Aufgabe ein, die in deiner Aufgabenliste hinzugefügt werden soll“. 
+    ## 2. Speichere die Eingabe in der Variablen `task`. 
+    ## 3. Füge die Aufgabe zur Liste hinzu. 
+    ## 4. Gib eine Meldung aus, dass die Aufgabe zur Liste hinzugefügt wurde. 
+
+    def add_task(self):
+        while True:
+            task = input("Please enter a Task: ")
+            if task:
+                while True:
+                    date_input = input("Please enter a Date in YYYY-MM-DD format: ")
+                    try:
+                        date_entered = datetime.datetime.strptime(date_input, '%Y-%m-%d').date()
+                        self.__todoList.append({"task":task, "date": date_entered})
+                        print(self.__todoList)
+                        return
+                    except:
+                        print("Please enter a real Date")
+                        
+            
+        
+
+    # Feature 2 show tasklist
+    
+    ## 1. Prüfe, ob die Liste leer ist. Wenn ja, gib den Text „Deine Aufgabenliste ist leer“ aus. 
+    ## 2. Wenn die Liste nicht leer ist, drucke alle Aufgaben mit einer `for`-Schleife. 
+    ## 3. Gib auch das Fälligkeitsdatum (falls vorhanden) mit aus. 
+
+
+    # Feature 3 Main Menu
+    
+    ## 1. Erstelle eine `while`-Schleife, die den User fragt, ob er eine Aufgabe hinzufügen, 
+    # die Liste anzeigen oder das Programm beenden möchte. 
+    ## 2. Nutze eine `if/elif`-Bedingung, um die Eingabe des Users zu verarbeiten. 
